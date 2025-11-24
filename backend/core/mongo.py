acf7764c7cacf9348a51f4487f94ca9b2cfe9a50
Benpido <benpido@gmail.com>
2025-11-24 18:14:31 -0300

    Secrets from .env

--- backend/core/mongo.py
+++ backend/core/mongo.py
@@ -3,8 +3,18 @@ import os
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 MONGO_DB = os.getenv("MONGO_DB", "completopia_mongo")
+MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+MONGO_AUTH_SOURCE = os.getenv("MONGO_AUTH_SOURCE")
 
-client = MongoClient(MONGO_URI)
+mongo_client_kwargs = {}
+if MONGO_USERNAME and MONGO_PASSWORD:
+    mongo_client_kwargs["username"] = MONGO_USERNAME
+    mongo_client_kwargs["password"] = MONGO_PASSWORD
+    if MONGO_AUTH_SOURCE:
+        mongo_client_kwargs["authSource"] = MONGO_AUTH_SOURCE
+
+client = MongoClient(MONGO_URI, **mongo_client_kwargs)
 mongo_db = client[MONGO_DB]
 
 # Colección de Productos
--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 // La URL base de tu API de Django
-const baseURL =
-  import.meta.env.VITE_API_BASE_URL ?? 'http://127.0.0.1:8000/api/';
+const baseURL = import.meta.env.VITE_API_BASE_URL?? '/api/'
+  //import.meta.env.VITE_API_BASE_URL; //?? 'http://127.0.0.1:8000/api/';
 
 const api = axios.create({
   baseURL: baseURL,
